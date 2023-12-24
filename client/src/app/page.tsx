e31c1bacae4ee202baa4b56fa7695e485e25d262
escoutdoor <vanap387@gmail.com>
2023-12-24 20:50:26 +0200

    styles + get apartment by id using hook

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,10 @@
+'use client'
+
 import Home from '@/components/pages/home/Home'
+import { useFilterApartments } from '@/hooks/useFilterApartments'
 
 export default async function HomePage() {
-	return <Home />
+	const { apartments, isLoading } = useFilterApartments({})
+
+	return <Home apartments={apartments || []} />
 }
