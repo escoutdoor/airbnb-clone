2f64db59758c319344eb6e37349288a575313f3d
escoutdoor <vanap387@gmail.com>
2023-12-24 20:40:50 +0200

    updated: module added prisma provider and ability module

--- api/src/review/review.module.ts
+++ api/src/review/review.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { ReviewService } from './review.service';
-import { ReviewController } from './review.controller';
+import { Module } from '@nestjs/common'
+import { ReviewService } from './review.service'
+import { ReviewController } from './review.controller'
+import { PrismaService } from 'src/prisma.service'
+import { AbilityModule } from 'src/ability/ability.module'
 
 @Module({
-  controllers: [ReviewController],
-  providers: [ReviewService],
+	controllers: [ReviewController],
+	providers: [ReviewService, PrismaService],
+	imports: [AbilityModule],
 })
 export class ReviewModule {}
