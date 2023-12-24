e2dfc92864686ed35fae27b0c6c3023f960b3442
escoutdoor <vanap387@gmail.com>
2023-12-24 20:50:58 +0200

    next.config.js updated: images remote pattern added

--- client/next.config.js
+++ client/next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
 	env: {
 		API_URL: process.env.API_URL,
 	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'a0.muscache.com',
+			},
+		],
+	},
 }
 
 module.exports = nextConfig
