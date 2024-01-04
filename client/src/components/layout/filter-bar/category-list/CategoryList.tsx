03ca3aff5641c033dd54243cfbe7f137b8f900a9
escoutdoor <vanap387@gmail.com>
2024-01-04 19:45:53 +0200

    updated way passing queries into url

--- client/src/components/layout/filter-bar/category-list/CategoryList.tsx
+++ client/src/components/layout/filter-bar/category-list/CategoryList.tsx
@@ -3,16 +3,17 @@
 import styles from './category-list.module.scss'
 import { FC } from 'react'
 import { ICategory } from '@/shared/interfaces/category.interface'
-import { useSearchParams } from 'next/navigation'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { useCreateQuery } from '@/hooks/useCreateQuery'
 import CategoryItem from './CategoryItem'
 
 const CategoryList: FC<{ categories: ICategory[] }> = ({ categories }) => {
 	const { createQuery } = useCreateQuery()
+	const { push } = useRouter()
 
 	const { get } = useSearchParams()
 
-	const activeCategory = get('category') || 'Arctic'
+	const activeCategory = get('category')
 
 	return (
 		<ul className={styles.list}>
@@ -21,12 +22,14 @@ const CategoryList: FC<{ categories: ICategory[] }> = ({ categories }) => {
 					key={category.id}
 					item={category}
 					isActive={activeCategory === category.name}
-					onClick={() => {
-						createQuery({
-							name: 'category',
-							value: category.name,
-						})
-					}}
+					onClick={() =>
+						push(
+							createQuery({
+								name: 'category',
+								value: category.name,
+							})
+						)
+					}
 				/>
 			))}
 		</ul>
