74236873737676244c03ce287a0a8ef345d25996
escoutdoor <vanap387@gmail.com>
2024-01-04 19:44:49 +0200

    updated: passing searchParams in home page

--- client/src/app/(home)/page.tsx
+++ client/src/app/(home)/page.tsx
@@ -3,8 +3,14 @@
 import Home from '@/components/pages/home/Home'
 import { useFilterApartments } from '@/hooks/useFilterApartments'
 
-export default function HomePage() {
-	const { apartments, isLoading } = useFilterApartments({})
+export default function HomePage({
+	searchParams,
+}: {
+	searchParams?: { [key: string]: string | string[] | undefined }
+}) {
+	const { apartments, isLoading } = useFilterApartments({
+		...searchParams,
+	})
 
 	return <Home apartments={apartments || []} />
 }
